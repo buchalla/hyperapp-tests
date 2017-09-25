dedb7bf6912fb2ae57c8439877da840f4682f563
Victor Schulze Buchalla <vs_buchalla@hotmail.com>
2017-09-24 23:38:14 -0300

    xd

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  entry: "./app/index.js",
+  output: {
+    filename: "./build/bundle.js",
+    path: path.resolve(__dirname, './'),
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: "babel-loader"
+    }]
+  }
+}
